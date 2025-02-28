bcd929e5c88839ec53bebd070b67e11d0cf7a84d
Aosaf098 <aosafibad10@gmail.com>
2025-02-28 09:24:51 +0600

    User Get

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,8 @@ const User = require("../models/User");
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find().select("-password");
-    res.josn(users);
+    const users = await User.find();
+    res.json(users);
   } catch (err) {
     console.log(err.message);
     res.status(500).send("User Not Found");
--- src/models/User.js
+++ src/models/User.js
@@ -10,6 +10,9 @@ const UserSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    firebase_UID: {
+        type: String,
+    },
     createdAt: {
         type: Date,
         default: Date.now
